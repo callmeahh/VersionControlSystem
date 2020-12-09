e6a7174083f508df4dbefdea489cc4390bb0add1
AHH <821227019@qq.com>
2020-12-09 11:14:21 +0800

    update part1

--- src/program/GetHashSHA1.java
+++ src/program/GetHashSHA1.java
@@ -4,8 +4,8 @@ import java.io.InputStream;
 import java.security.MessageDigest;
 
 public class GetHashSHA1 {
+	// 将字符串通过SHA-1方法哈希
 	public static String getFileHash(InputStream is) throws Exception {
-//		is.mark(0);
 		byte[] buffer = new byte[1024];
 		MessageDigest complete = MessageDigest.getInstance("SHA-1");
 		int numRead = 0;
@@ -15,7 +15,6 @@ public class GetHashSHA1 {
 				complete.update(buffer, 0, numRead);
 			}
 		} while (numRead != -1);
-//		is.reset();
 		return bytesToHexString(complete.digest());
 	}
 
@@ -26,6 +25,7 @@ public class GetHashSHA1 {
 		return bytesToHexString(complete.digest());
 	}
 
+	// 将数组类型转为字符串
 	private static String bytesToHexString(byte[] b) {
 		String hashString = "";
 		for (int j = 0; j < b.length; j++) {
--- src/program/blobKey.java
+++ src/program/blobKey.java
@@ -9,13 +9,16 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 
 public class blobKey {
+	// 设置文件存放路径
 	private static String filePar = "E:\\JavaWorkspace\\VersionRepository";
 
+	// 获取路径下所有文件的地址
 	private static File[] getFileList() {
 		File dir = new File(filePar);
 		return dir.listFiles();
 	}
 
+	// 字符串匹配
 	private static int findSubstring(File[] fl, String s2) {
 		int n = 0;
 		int index = -1;
@@ -32,11 +35,12 @@ public class blobKey {
 			}
 		}
 		if (n == 1)
-			return index;
+			return index; // 找到一个匹配值，返回该下标
 		else
-			return -1;
+			return -1; // 找到其他匹配值，返回-1
 	}
 
+	// 将blob类型文件存入本地
 	public static void recordBlob(String filename) throws Exception {
 		File file = new File(filename);
 		BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
@@ -52,7 +56,17 @@ public class blobKey {
 		System.out.println("执行完毕，文件导出到 " + outputPath);
 	}
 
-	public static void getValue(String hash) throws Exception {
+	// 将tree类型文件夹存入本地
+	public static void recordTree(String filename, String content) throws Exception {
+		String outputPath = filePar + File.separator + filename;
+		BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(outputPath));
+		bos.write(content.getBytes());
+		bos.close();
+		System.out.println("执行完毕，文件导出到 " + outputPath);
+	}
+
+	// 通过key查找value
+	public static void searchValue(String hash) throws Exception {
 		File[] fl = getFileList();
 		int flag = findSubstring(fl, hash);
 		if (flag != -1) {
